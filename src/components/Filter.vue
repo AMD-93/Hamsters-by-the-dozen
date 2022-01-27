4c1a0b69dc82db387c0e83ff81c28f290d22e63d
AMD-93 <amoyad93@gmail.com>
2022-01-27 18:57:10 +0100

    filter component added and small changes

---
+++ src/components/Filter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "Filter",
+    props: {
+        filter: Array
+    }, 
+    methods: {
+        filterFilms() {
+            
+        }
+    },
+    created() {
+        
+    }
+}
+</script>
+
+
+<template>
+    <form>
+        <input type="text" v-model="filter" placeholder="Search films by title" @keyup="filterFilms">
+    </form>
+
+</template>
+
+
+<style>
+
+</style>
